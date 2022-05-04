f11967af00a377f5b8ef3e12af0f92fbf0ffc3ee
Junayed6 <abutahirjunayed@gmail.com>
2022-05-04 10:38:51 +0600

    Product component add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
+import Products from '../products/Products';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Products/>
             <Footer/>
         </div>
     );
