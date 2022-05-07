9bc65f8fa73ac219e0257df5e846020ac98cf4e8
Junayed6 <abutahirjunayed@gmail.com>
2022-05-07 11:18:38 +0600

    delivered

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,22 +3,23 @@ import { useParams } from 'react-router-dom';
 
 const Item = () => {
     const searchId = useParams();
+    const id = searchId.productId;
     const [product, setProduct] = useState({});
-    const {_id,img,name,price,quantity,description,supplier} = product;
+    const { _id, img, name, price, quantity, description, supplier } = product;
+    
     useEffect(() => {
-        const id = searchId.productId;
         const url = `https://protected-coast-77549.herokuapp.com/product/${id}`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setProduct(data));
+            .then(res => res.json())
+            .then(data => setProduct(data));
     }, [product]);
 
-    
+
 
     const handleDelivered = () => {
         // console.log(id);
         // const productId = id;
-        const productQuantity = {quantity:quantity - 1}
+        const productQuantity = { quantity: quantity - 1 }
 
         const url = `https://protected-coast-77549.herokuapp.com/products/${_id}`;
         fetch(url, {
@@ -35,16 +36,31 @@ const Item = () => {
             })
     }
 
+    const handleRestock = event => {
+        event.preventDefault();
+        const restock = event.target.restock.value;
+        console.log(restock);
+    }
+
     return (
-        <div className='border w-25 mx-auto text-start'>
-            <img className='w-100' src={img} alt="" />
-            <p className='mb-1'><small>Product ID:{_id}</small></p>
-            <h4 className='text-center'>Name:{name}</h4>
-            <p className='mb-1'>Price:{price}</p>
-            <p className='mb-1'>Quantity:{quantity}</p>
-            <p className='mb-1'>Supplier:{supplier}</p>
-            <p><small>Description:{description}</small></p>
-            <button className='btn btn-info w-100' onClick={handleDelivered}>Delivered</button>
+        <div>
+            <div className='border w-25 mx-auto text-start'>
+                <img className='w-100' src={img} alt="" />
+                <p className='mb-1'><small>Product ID:{_id}</small></p>
+                <h4 className='text-center'>Name:{name}</h4>
+                <p className='mb-1'>Price:{price}</p>
+                <p className='mb-1'>Quantity:{quantity}</p>
+                <p className='mb-1'>Supplier:{supplier}</p>
+                <p><small>Description:{description}</small></p>
+                <button className='btn btn-info w-100' onClick={handleDelivered}>Delivered</button>
+            </div>
+
+            <div className='mt-5 w-50 mx-auto'>
+                <form onSubmit={handleRestock}>
+                    <input className='w-50' type="text" name="restock" id="" />
+                    <input type="submit" value="Restock" />
+                </form>
+            </div>
         </div>
     );
 };
