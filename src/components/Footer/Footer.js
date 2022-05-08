08251c5245a50c7760090fd23cf389203f9eef51
Junayed6 <abutahirjunayed@gmail.com>
2022-05-08 12:23:23 +0600

    footer set

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import facebook from '../../icon/facebook.png';
+import github from '../../icon/github.png';
+import google from '../../icon/google.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer always theng</h2>
+        <div className='bg-light'>
+            <h2>Contact us</h2>
+            <div>
+                <a href="https://www.google.com/"><img width={30} className='m-2' src={google} alt="" /></a>
+                <a href="https://www.facebook.com/"><img width={30} className='m-2' src={facebook} alt="" /></a>
+                <a href="https://github.com/"><img width={30} className='m-2' src={github} alt="" /></a>
+            </div>
         </div>
     );
 };
